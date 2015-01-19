83efd60645462b9d57b681b818c716afda080292
Kristofer Joseph <kristoferjoseph@gmail.com>
2015-01-19 11:52:08 -0800

    Refactoring to early return.

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 "use strict";
 
 module.exports = function () {
-  if (typeof HTMLTemplateElement === "undefined") {
-    (function () {
-      HTMLTemplateElement = function () {};
-      HTMLTemplateElement.prototype = Object.create(HTMLElement.prototype);
+  if (typeof HTMLTemplateElement !== "undefined") {
+    return;
+  }
 
-      HTMLTemplateElement.decorate = function (template) {
-        if (!template.content) {
-          template.content = template.ownerDocument.createDocumentFragment();
-          var child;
-          while (child = template.firstChild) {
-            template.content.appendChild(child);
-          }
+  (function () {
+    HTMLTemplateElement = function () {};
+    HTMLTemplateElement.prototype = Object.create(HTMLElement.prototype);
+
+    HTMLTemplateElement.decorate = function (template) {
+      if (!template.content) {
+        template.content = template.ownerDocument.createDocumentFragment();
+        var child;
+        while (child = template.firstChild) {
+          template.content.appendChild(child);
         }
-      };
+      }
+    };
 
-      HTMLTemplateElement.initialize = function (doc) {
-        var templates = doc.querySelector("template");
-        for (var i = 0, l = templates.length, t; i < l && (t = templates[i]); i++) {
-          HTMLTemplateElement.decorate(t);
-        };
+    HTMLTemplateElement.initialize = function (doc) {
+      var templates = doc.querySelector("template");
+      for (var i = 0, l = templates.length, t; i < l && (t = templates[i]); i++) {
+        HTMLTemplateElement.decorate(t);
       };
+    };
 
-      addEventListener("DOMContentLoaded", function () {
-        HTMLTemplateElement.initialize(document);
-      });
-    })();
-  };
+    addEventListener("DOMContentLoaded", function () {
+      HTMLTemplateElement.initialize(document);
+    });
+  })();
 };
